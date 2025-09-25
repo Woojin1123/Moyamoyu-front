1fe9cf1cb3408ae751bcb916bd8da4e2d9d88835
Woojin1123 <boom175839@gmail.com>
2025-09-25 12:58:30 +0900

    fix: alias

--- moyamoyu-front/vite.config.js
+++ moyamoyu-front/vite.config.js
@@ -2,17 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { fileURLToPath } from "url";
 
-// __dirname 대체
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
   },
 });
